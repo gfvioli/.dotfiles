4292eb0e2c54ee30b5ff3cbcb7d2d5894aec44a1
Gian Violi <96206151+gfvioli@users.noreply.github.com>
2024-07-17 08:32:30 +0200

    adding more quarto configuration
    file types and double checking some configuration details

--- .config/nvim/lua/gfvioli/plugins/quarto.lua
+++ .config/nvim/lua/gfvioli/plugins/quarto.lua
@@ -2,15 +2,20 @@ return {
     {
         'quarto-dev/quarto-nvim',
         dependencies = {
-            'jmbuhr/otter.nvim',
-            opts = {},
+            {
+                'jmbuhr/otter.nvim',
+                opts = {},
+                dev = false,
+                dependencies = {
+                    'neovim/nvim-lspconfig',
+                    'nvim-treesitter/nvim-treesitter',
+                }
+            },
         },
-        config = function()
-        end,
+        ft = { 'quarto' },
+        dev = false,
     },
-    {
-        --directly open ipynb files as quarto docuements
-        -- and convert back behind the scenes
+    { --directly open ipynb files as quarto docuements and convert back behind the scenes
         'GCBallesteros/jupytext.nvim',
         opts = {
             custom_language_formatting = {
@@ -27,9 +32,7 @@ return {
             },
         },
     },
-    {
-        -- send code from python/r/qmd documets to a terminal or REPL
-        -- like ipython, R, bash
+    { -- send code from python/r/qmd documets to a terminal or REPL like ipython, R, bash
         'jpalardy/vim-slime',
         dev = false,
         init = function()
@@ -76,11 +79,60 @@ return {
             vim.keymap.set('n', '<leader>cs', set_terminal, { desc = '[s]et terminal' })
         end,
     },
-    {
-        -- preview equations
+    { -- preview equations
         'jbyuki/nabla.nvim',
         keys = {
             { '<leader>qm', ':lua require"nabla".toggle_virt()<cr>', desc = 'toggle [M]ath equations' }
         }
-    }
+    },
+    { -- paste an image from the clipboard or drag-and-drop
+        'HakonHarnes/img-clip.nvim',
+        event = 'BufEnter',
+        ft = { 'markdown', 'quarto', 'latex' },
+        opts = {
+            default = {
+                dir_path = 'img',
+            },
+            filetypes = {
+                markdown = {
+                    url_encode_path = true,
+                    template = '![$CURSOR]($FILE_PATH)',
+                    drag_and_drop = {
+                        download_images = false,
+                    },
+                },
+                quarto = {
+                    url_encode_path = true,
+                    template = '![$CURSOR]($FILE_PATH)',
+                    drag_and_drop = {
+                        download_images = false,
+                    },
+                },
+            },
+        },
+        config = function(_, opts)
+            require('img-clip').setup(opts)
+            vim.keymap.set('n', '<leader>ii', ':PasteImage<cr>', { desc = 'insert [i]mage from clipboard' })
+        end,
+    },
+    {
+        'benlubas/molten-nvim',
+        enabled = false,
+        build = ':UpdateRemotePlugins',
+        init = function()
+            vim.g.molten_image_provider = 'image.nvim'
+            vim.g.molten_output_win_max_height = 20
+            vim.g.molten_auto_open_output = false
+        end,
+        keys = {
+            { '<leader>mi', ':MoltenInit<cr>',           desc = '[m]olten [i]nit' },
+            {
+                '<leader>mv',
+                ':<C-u>MoltenEvaluateVisual<cr>',
+                mode = 'v',
+                desc = 'molten eval visual',
+            },
+            { '<leader>mr', ':MoltenReevaluateCell<cr>', desc = 'molten re-eval cell' },
+        },
+    },
 }
