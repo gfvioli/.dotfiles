54adc406663df8ef3c991bcff9a841395fdd9ecb
Gian Violi <96206151+gfvioli@users.noreply.github.com>
2025-10-09 10:07:56 +0200

    dap fixed

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -1,7 +1,34 @@
 return {
   "mfussenegger/nvim-dap",
-  table.insert(require("nvim-dap").configurations.python, {
+  optional = false,
+  lazy = false,
+  dependencies = {
     {
+      "mfussenegger/nvim-dap-python",
+      keys = {
+        {
+          "<leader>dPt",
+          function()
+            require("dap-python").test_method()
+          end,
+          desc = "Debug Method",
+          ft = "python",
+        },
+        {
+          "<leader>dPc",
+          function()
+            require("dap-python").test_class()
+          end,
+          desc = "Debug Class",
+          ft = "python",
+        },
+      },
+    },
+  },
+  config = function()
+    local dap = require("dap")
+    require("dap-python").setup("uv")
+    table.insert(dap.configurations.python, {
       name = "Python Current File",
       type = "debugpy",
       request = "launch",
@@ -10,8 +37,8 @@ return {
       },
       program = "${file}",
       pythonPath = ".venv/bin/python",
-    },
-    {
+    })
+    table.insert(dap.configurations.python, {
       name = "dagster-dev",
       type = "debugpy",
       request = "launch",
@@ -22,6 +49,6 @@ return {
       },
       subProcess = true,
       pythonPath = ".venv/bin/python",
-    },
-  }),
+    })
+  end,
 }
